a77bfe2b6b0c4921cb70561b5e5a6c15098adb8d
Jack Huddlestone <jack@itsjack.cloud>
2024-11-30 23:12:48 +0000

    output law

--- modules/container_app_environment/outputs.tf
+++ modules/container_app_environment/outputs.tf
@@ -7,3 +7,8 @@ output "default_domain_name" {
   description = "The default domain name of the container app environment."
   value       = azurerm_container_app_environment.container_app_environment.default_domain
 }
+
+output "log_analytics_workspace_id" {
+  description = "The ID of the Log Analytics workspace associated with the container app environment."
+  value       = azurerm_container_app_environment.container_app_environment.log_analytics_workspace_id
+}
