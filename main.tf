afdee01c74fe8042f8828a203a264f24d10fc417
Jack Huddlestone <jack@itsjack.cloud>
2024-11-30 20:15:37 +0000

    reference rg attributes

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "container_app_environment" {
   source                         = "./modules/container_app_environment"
   container_app_environment_name = var.container_app_environment_name
   resource_group_name            = azurerm_resource_group.resource_group.name
-  location                       = var.location
+  location                       = azurerm_resource_group.resource_group.location
   log_analytics_workspace_name   = var.log_analytics_workspace_name
   tags                           = var.tags
 }
