96faef162ebdd26e3b39de494cdd3843262cc544
Jack Huddlestone <jack@itsjack.cloud>
2024-11-30 21:21:12 +0000

    add storage share

--- modules/container_grafana/main.tf
+++ modules/container_grafana/main.tf
@@ -23,6 +23,21 @@ resource "azurerm_container_app" "grafana" {
   tags = var.tags
 }
 
+resource "azurerm_storage_account" "azurerm_storage_account" {
+  name                     = var.grafana_storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags                     = var.tags
+}
+
+resource "azurerm_storage_share" "azurerm_storage_share" {
+  name               = "grafana"
+  storage_account_id = azurerm_storage_account.azurerm_storage_account.id
+  quota              = 10
+}
+
 resource "azurerm_user_assigned_identity" "user_assigned_identity" {
   name                = var.grafana_user_assigned_identity_name
   resource_group_name = var.resource_group_name
--- modules/container_grafana/variables.tf
+++ modules/container_grafana/variables.tf
@@ -14,6 +14,10 @@ variable "resource_group_name" {
   description = "The name of the resource group in which the grafana will be deployed."
 }
 
+variable "grafana_storage_account_name" {
+  description = "The name of the storage account to be created for the grafana container."
+}
+
 variable "tags" {
   description = "A mapping of tags to assign to the grafana container."
   type        = map(string)
