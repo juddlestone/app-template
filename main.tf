f366099346a76430cc6f2ce0a8772eece59479b9
Jack Huddlestone <jack@itsjack.cloud>
2024-12-01 21:30:09 +0000

    add explicity dependency

--- main.tf
+++ main.tf
@@ -26,4 +26,6 @@ module "grafana" {
   resource_group_name                 = azurerm_resource_group.resource_group.name
   location                            = azurerm_resource_group.resource_group.location
   tags                                = var.tags
+
+  depends_on = [ module.container_app_environment ]
 }
