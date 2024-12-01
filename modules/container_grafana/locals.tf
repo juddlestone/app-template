7a30da53f312cb5f7580440b345d092ebba20b7b
Jack Huddlestone <jack@itsjack.cloud>
2024-12-01 12:12:26 +0000

    add query retires

--- modules/container_grafana/locals.tf
+++ modules/container_grafana/locals.tf
@@ -5,6 +5,7 @@ locals {
     "GF_AZURE_MANAGED_IDENTITY_ENABLED"   = true
     "GF_AZURE_MANAGED_IDENTITY_CLIENT_ID" = azurerm_user_assigned_identity.user_assigned_identity.client_id
     "GF_DATABASE_MIGRATION_LOCKING"       = false
+    "GF_DATABASE_QUERY_RETRIES"           = 10
   }
 
   subscription_id = "/subscriptions/${data.azurerm_subscription.current.subscription_id}"
